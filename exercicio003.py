c3f2252c3c3509b0225753fe2b6936e0665a9209
Fagner Oliveira <fagnerbto@gmail.com>
2022-03-12 15:18:14 -0300

    03 - adding two integers and printing the result number.

---
+++ exercicio003.py
@@ -0,0 +1,10 @@
+"""
+003
+Fornecendo dois números, retorne a soma
+"""
+print("Informe 2 números inteiros, conforme o sistema solicitar.")
+print("O sistema exibirá o resultado da soma dos dois números.")
+primeiro_numero = int(input('Forneça o primeiro número e aperte a tecla enter:'))
+segundo_numero = int(input('Forneça o segundo número e aperte a tecla enter:'))
+resultado = primeiro_numero + segundo_numero
+print(f'A soma dos números {primeiro_numero} e {segundo_numero} é de {resultado}')
