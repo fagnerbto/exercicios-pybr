7bf7a34fa0a0bc23c274f24d98252c8ac71baa54
Fagner Oliveira <fagnerbto@gmail.com>
2022-03-12 23:10:12 -0300

    006 - calcula área de um círculo

--- exercicio006.py
+++ exercicio006.py
@@ -2,3 +2,11 @@
 006 
 Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
 """
+import math
+
+
+raio = int(input('Este programa calcula a área de um círculo. Entre com o valor de raio desejado:'))
+
+area = math.pi * (raio **2)
+
+print(f'A área de um círculo de raio {raio} é {area}')
