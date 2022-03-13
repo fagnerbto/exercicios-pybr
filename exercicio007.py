7a8c4a545448bf538f30f06aa21fbb509b9a7153
Fagner Oliveira <fagnerbto@gmail.com>
2022-03-12 23:15:46 -0300

    007 - calculo de área do quadrado

---
+++ exercicio007.py
@@ -0,0 +1,8 @@
+"""
+007
+Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+"""
+lado = int(input('Este programa calcula a área de um quadrado. Forneça o valor do lado:'))
+
+area = lado ** 2
+print(f'A área do quadrado é: {area}. O dobro da área é {area*2}')
