b84467ac8846d1fab8ae3d08b53d2d73b6685d0c
Fagner Oliveira <fagnerbto@gmail.com>
2022-03-12 22:47:49 -0300

    005 - Convertendo de metros para centímetros

---
+++ exercicio005.py
@@ -0,0 +1,9 @@
+"""
+005 - Convertendo metros para centímetros
+"""
+
+valor_em_metros = int(input("Forneça o valor em metros que deseja converter para centímetros:"))
+
+valor_em_centimetros = valor_em_metros * 1000
+
+print(f'{valor_em_metros} em metros equivale a {valor_em_centimetros} centímetros')
