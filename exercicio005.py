6d5b44936ea29f260c1523e96f7421b91c3059b3
Fagner Oliveira <fagnerbto@gmail.com>
2022-03-13 23:17:56 -0300

    005 - Corrigindo o multiplicador

--- exercicio005.py
+++ exercicio005.py
@@ -5,6 +5,6 @@ Faça um Programa que converta metros para centímetros.
 
 valor_em_metros = int(input("Forneça o valor em metros que deseja converter para centímetros:"))
 
-valor_em_centimetros = valor_em_metros * 1000
+valor_em_centimetros = valor_em_metros * 100
 
 print(f'{valor_em_metros} em metros equivale a {valor_em_centimetros} centímetros')
